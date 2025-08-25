4cebd1ef56852748eeabab5a75ae9f4d1dae97f4
Mateus Melo - Dev <mateusfpmelodev@gmail.com>
2025-08-25 16:55:57 -0300

    base

--- src/components/formsection.tsx
+++ src/components/formsection.tsx
@@ -4,9 +4,46 @@ import { X } from "lucide-react";
 
 export default function FormSection() {
     const [showForm, setShowForm] = useState(false)
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [message, setMessage] = useState("")
+    const [btnLoading, setBtnLoading] = useState(false)
 
+    type ApiResponse = {
+        status: number;
+        ok: boolean;
+        body: any; // você pode trocar `any` por um tipo mais específico depois
+    };
+
+    const [returnReq, setReturnReq] = useState<ApiResponse | null>(null)
+
+    const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setReturnReq(null)
+    setBtnLoading(true)
+
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ name, email, message }),
+    });
+
+    const data = await response.json();
+
+    const fullResponse: ApiResponse = {
+        status: response.status,
+        ok: response.ok,
+        body: data,
+    };
+
+    setReturnReq(fullResponse);
+    setName("")
+    setEmail("")
+    setMessage("")
+    setBtnLoading(false)
+    };
     return(
-        <section id="form-section" className="w-full min-h-[40vh] flex items-center justify-center bg-[#fff] text-[#131313] p-8">
+        <section id="form-section" className="w-full min-h-[40vh] flex items-center justify-center bg-[#fff] text-[#131313] py-16 px-8">
             <div className="w-full max-w-[1200px] flex flex-col gap-2">
                 <h2 className="text-xl font-bold opacity-0 animate-zoomIn">Preencha o <span className="text-[#B727F5]">Formulário</span></h2>
                 <p>Este projeto é uma mini landing page criada para demonstrar o uso de testes automatizados em um ambiente React + TypeScript + Tailwind. O objetivo é mostrar como aplicar boas práticas de <b>qualidade de software</b>.</p>
@@ -17,17 +54,43 @@ export default function FormSection() {
 
                             <div className="w-full flex justify-end">
                                 <button
-                                    onClick={() => setShowForm(false)} aria-label="Fechar formulário" className="rounded-full bg-[#C7C7C7] hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-400" >
+                                    onClick={() => setShowForm(false)} aria-label="Fechar formulário" className="rounded-full bg-[#C7C7C7] hover:bg-gray-800 focus:outline-none focus:ring-2 " >
                                     <X className="w-6 h-6 text-[#131313] hover:text-[#fff]" />
                                 </button>
                             </div>
-                            <form className="flex flex-col gap-4 bg-[#2a2a2a] p-6 rounded">
-                                <input
-                                type="text" placeholder="Seu nome" className="p-2 rounded text-black" />
-                                <input type="email" placeholder="Seu email" className="p-2 rounded text-black" />
-                                <Button type="submit">Enviar</Button>
+                            <form className="flex flex-col gap-4 p-6 rounded" onSubmit={handleSubmit}>
+                                <input type="text" id="name" value={name} onChange={(e)=> setName(e.target.value)} placeholder="Seu nome" aria-label="Seu nome" className="p-2 rounded text-[#121212] border-2 [#131313] focus:ring-2 focus:ring-[#FF00F2]" />
+                                <input type="email" id="email" value={email} onChange={(e)=> setEmail(e.target.value)} placeholder="Seu email" aria-label="Seu email" className="p-2 rounded text-[#121212] border-2 [#131313] focus:ring-2 focus:ring-[#FF00F2]"/>
+                                <input type="text" id="message" value={message} onChange={(e)=> setMessage(e.target.value)} placeholder="Sua mensagem" aria-label="Sua mensagem" className="p-2 rounded text-[#121212] border-2 [#131313] focus:ring-2 focus:ring-[#FF00F2]"/>
+                                <Button disabled={btnLoading} type="submit">Enviar</Button>
+                                {returnReq && (
+                                    <div className="w-full flex justify-start">
+                                        <div
+                                        className={`w-full rounded-2xl px-4 py-3 shadow-md border ${
+                                            returnReq.ok
+                                            ? "bg-[#E6FFFA] border-[#38B2AC] text-[#285E61]" 
+                                            : "bg-[#FFF5F5] border-[#F56565] text-[#C53030]"
+                                        }`}
+                                        >
+                                        {returnReq.ok ? (
+                                            <p>
+                                            ✅ Sua mensagem foi enviada com sucesso! <br />
+                                            <span className="font-semibold">Status:</span> {returnReq.status} <br />
+                                            <span className="font-semibold">ID retornado:</span>{" "}
+                                            {returnReq.body.id}
+                                            </p>
+                                        ) : (
+                                            <p>
+                                            ❌ Ocorreu um erro ao enviar sua mensagem. <br />
+                                            <span className="font-semibold">Status:</span> {returnReq.status}
+                                            </p>
+                                        )}
+                                        </div>
+                                    </div>
+                                )}
                             </form>
                         </>
+
                 )}
                 
             </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
 
     return(
         <header className="w-full min-h-[70vh] flex items-center justify-center bg-[#131313] text-[#fff] p-8">
-            <div className="w-full max-w-[700px] flex flex-col gap-2">
+            <div className="w-full max-w-[1200px] flex flex-col gap-2">
                 <h1 className="text-2xl font-bold opacity-0 animate-zoomIn">Testes Automatizados: Exemplo <span className="text-[#B727F5]">React</span> + TypeScript +  <span className="text-[#B727F5]">Tailwind</span></h1>
                 <p>Este projeto é uma mini landing page criada para demonstrar o uso de testes automatizados em um ambiente React + TypeScript + Tailwind. O objetivo é mostrar na prática <b>testes unitários</b> e <b>testes de integração</b>.</p>
                 <div><Button onClick={scrollToForm} children="Clique aqui para ir para o formulário" /></div>
